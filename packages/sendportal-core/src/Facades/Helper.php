b75e94d862cdfbe7d3f258222bde4dc946d8f191
Felipe Mateus <eu@felipemateus.com>
2025-05-02 02:42:38 -0300

    migrate sendportal core
    - migrate sendportal core
    - add suport to php 8.4
    - add suport to laravel 12

---
+++ packages/sendportal-core/src/Facades/Helper.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Sendportal\Base\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class Helper extends Facade
+{
+    /**
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return 'sendportal.helper';
+    }
+}
